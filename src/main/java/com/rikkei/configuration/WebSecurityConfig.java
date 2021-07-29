da72dbc31ee85eb191df5a02ff0a552aa039cb95
Chau <thaichau005@gmail.com>
2021-07-29 20:46:16 +0700

    Add Logout and Authorization

--- src/main/java/com/rikkei/configuration/WebSecurityConfig.java
+++ src/main/java/com/rikkei/configuration/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import com.rikkei.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 
@@ -35,7 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
-         
         return authProvider;
     }
 
@@ -47,12 +48,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic().disable();
     	http.authorizeRequests()
         .anyRequest().authenticated() // Tất cả các request khác đều cần phải xác thực mới được truy cập
         .and()
         .formLogin() // Cho phép người dùng xác thực bằng form login
-        .defaultSuccessUrl("/hello")
-        .permitAll(); // Tất cả đều được truy cập vào địa chỉ này
+        .defaultSuccessUrl("/index")
+        .permitAll()// Tất cả đều được truy cập vào địa chỉ này
+    	.and()
+    	.logout() // Cho phép logout
+    	.permitAll();
+
     }
 }
